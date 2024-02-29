2fb1d471d1c57cb2705604bc537be362d17dcdf0
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-29 15:39:08 +0330

    Theme & TextStyles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:blog_club/view/main_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'constant.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +13,65 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "Blog Club",
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: const CupertinoScrollBehavior(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+        useMaterial3: true,
+        fontFamily: "Avenir",
+        textTheme: const TextTheme(
+          titleSmall: TextStyle(
+            fontFamily: "Avenir",
+            color: primaryTextColor,
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+          titleMedium: TextStyle(
+            fontFamily: "Avenir",
+            color: secondaryTextColor,
+            fontSize: 18,
+            fontWeight: FontWeight.w200,
+          ),
+          titleLarge: TextStyle(
+            fontFamily: "Avenir",
+            fontWeight: FontWeight.bold,
+            color: primaryTextColor,
+            fontSize: 18,
+          ),
+          headlineMedium: TextStyle(
+            fontFamily: "Avenir",
+            fontWeight: FontWeight.w700,
+            fontSize: 24,
+            color: primaryTextColor,
+          ),
+          headlineSmall: TextStyle(
+            fontFamily: "Avenir",
+            fontSize: 20,
+            color: primaryTextColor,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyMedium: TextStyle(
+            fontFamily: "Avenir",
+            color: secondaryTextColor,
+            fontSize: 12,
+          ),
+          bodySmall: TextStyle(
+            fontFamily: "Avenir",
+            fontWeight: FontWeight.w700,
+            fontSize: 10,
+            color: Color(0xff7B8BB2),
+          ),
+          bodyLarge: TextStyle(
+            fontFamily: "Avenir",
+            color: primaryTextColor,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: MainScreen(),
+    );
   }
 }
 
