503513388d5a59a223a60ee71231fb15022fa831
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-29 15:37:21 +0330

    constant Color

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryTextColor = Color(0xff0D253C);
+const secondaryTextColor = Color(0xff2D4379);
+const primaryColor = Color(0xFF376AED);
+const greyColor = Color(0xff7B8BB2);
+const backgroundColor = Color(0xFFFBFBFF);
