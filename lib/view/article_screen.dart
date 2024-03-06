182c67294ab4c0627e16d496f3a95eb17e013a76
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-06 11:50:47 +0330

    Article Screen profile detail

--- lib/view/article_screen.dart
+++ lib/view/article_screen.dart
@@ -1,3 +1,4 @@
+import 'package:blog_club/constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -38,8 +39,60 @@ class _ArticleScreenState extends State<ArticleScreen> {
               SliverList(
                 delegate: SliverChildListDelegate.fixed([
                   Padding(
-                    padding: EdgeInsets.all(25),
-                    child: Column(),
+                    padding: const EdgeInsets.all(25),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Four things Every Woman Needs To Know",
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(35),
+                              child: Image.asset(
+                                "assets/img/stories/story_5.jpg",
+                                width: 55,
+                                height: 55,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Richard Gervain",
+                                  style: Theme.of(context).textTheme.bodyLarge,
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                const Text("2m ago"),
+                              ],
+                            ),
+                            const Spacer(),
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.send_outlined,
+                                  color: primaryColor,
+                                )),
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.bookmark_outline,
+                                  color: primaryColor,
+                                )),
+                          ],
+                        ),
+                      ],
+                    ),
                   )
                 ]),
               ),
