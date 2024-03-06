1b524f5ba358102d4ed04be4b460586bb6bfde2c
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-06 11:39:37 +0330

    Article Screen App bar

--- lib/view/article_screen.dart
+++ lib/view/article_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ArticleScreen extends StatefulWidget {
@@ -10,6 +11,42 @@ class ArticleScreen extends StatefulWidget {
 class _ArticleScreenState extends State<ArticleScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                leadingWidth: 100,
+                leading: IconButton(
+                  onPressed: () {},
+                  iconSize: 30,
+                  icon: const Icon(CupertinoIcons.chevron_back),
+                ),
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 25),
+                    child: IconButton(
+                      onPressed: () {},
+                      iconSize: 35,
+                      icon: const Icon(Icons.more_horiz),
+                    ),
+                  ),
+                ],
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate.fixed([
+                  Padding(
+                    padding: EdgeInsets.all(25),
+                    child: Column(),
+                  )
+                ]),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
