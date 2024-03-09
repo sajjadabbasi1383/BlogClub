f0d99f8ba46141e7802695cff9161de205c6b58d
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-09 17:27:19 +0330

    OnBoarding model

---
+++ lib/model/onboarding_model.dart
@@ -0,0 +1,13 @@
+class OnBoardingItem{
+  final String title;
+  final String description;
+
+  const OnBoardingItem({required this.title, required this.description});
+}
+
+const List<OnBoardingItem> onBoardingItems=[
+  OnBoardingItem(title: "title", description: "description"),
+  OnBoardingItem(title: "title", description: "description"),
+  OnBoardingItem(title: "title", description: "description"),
+  OnBoardingItem(title: "title", description: "description"),
+];
