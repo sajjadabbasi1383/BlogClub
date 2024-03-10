4605baa8d636aad030bbab82c60332c76fd50eca
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-10 14:54:56 +0330

    Authentication Screen tab controller

--- lib/view/authentication_screen.dart
+++ lib/view/authentication_screen.dart
@@ -12,17 +12,62 @@ class AuthenticationScreen extends StatefulWidget {
 class _AuthenticationScreenState extends State<AuthenticationScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+        child: Scaffold(
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 15,bottom: 15),
-            child: SvgPicture.asset("assets/img/icons/LOGO.svg"),
+            padding: const EdgeInsets.only(top: 25, bottom: 25),
+            child: SvgPicture.asset(
+              "assets/img/icons/LOGO.svg",
+              width: 130,
+            ),
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: primaryColor,
-
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30))),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "LOGIN",
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .apply(color: Colors.white),
+                        ),
+                        Text(
+                          "SIGN UP",
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .apply(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: backgroundColor,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30))),
+                      child: Column(
+                        children: [],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
