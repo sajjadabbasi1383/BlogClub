1f0c99c9eaa9bd92205c5c5c7ac6b42eaf37fa5f
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-10 17:00:19 +0330

    add screenshot

--- lib/model/onboarding_model.dart
+++ lib/model/onboarding_model.dart
@@ -6,8 +6,8 @@ class OnBoardingItem{
 }
 
 const List<OnBoardingItem> onBoardingItems=[
-  OnBoardingItem(title: "title", description: "description"),
-  OnBoardingItem(title: "title", description: "description"),
-  OnBoardingItem(title: "title", description: "description"),
-  OnBoardingItem(title: "title", description: "description"),
+  OnBoardingItem(title: "Read the article you want instantly", description: "You can read thousands of articles on Blog Club, save them in the application and share them with your loved ones."),
+  OnBoardingItem(title: "Write your favorite article", description: "You can read thousands of articles on Blog Club, save them in the application and share them with your loved ones."),
+  OnBoardingItem(title: "Follow other people's articles", description: "You can read thousands of articles on Blog Club, save them in the application and share them with your loved ones."),
+  OnBoardingItem(title: "Share your articles with others", description: "You can read thousands of articles on Blog Club, save them in the application and share them with your loved ones."),
 ];
